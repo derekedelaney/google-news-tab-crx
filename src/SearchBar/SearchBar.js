45b4c970bd0e71276a431b325d94f80fd53311e6
Derek Delaney <derekdelaney@Dereks-MacBook-Pro.local>
2019-06-10 23:21:10 -0700

    search field works

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function SearchBar() {
-    const handleKeyDown = (e) => {
-        focusAddressBar();
-        e.target.blur();
-    };
-    return <div className="google-search-container">
-        <div className="google-search-box">
-            <input type="text" placeholder="Search Google or type a Url" className="browser-default google-search-input" onKeyDown={handleKeyDown} />
+    const [searchText, onSearchTextChange] = useState('');
+    function handleChange(e) {
+        onSearchTextChange(e.target.value);
+    }
+    function handleKeyDown(e) {
+        if (e.key === "Enter") {
+            window.open(`http://www.google.com/search?q=${encodeURIComponent(searchText)}`, '_top')
+        }
+    }
+    return (
+        <div className="google-search-container">
+            <div className="google-search-box">
+                <input
+                    type="text"
+                    placeholder="Search Google or type a Url"
+                    className="browser-default google-search-input"
+                    onChange={handleChange}
+                    onKeyDown={handleKeyDown}
+                    value={searchText}
+                />
+            </div>
         </div>
-    </div>
-}
-
-function focusAddressBar() {
-    var keyEvent = new KeyboardEvent('keypress', {key: 'l', char: 'l', code: 'KeyL', metaKey: true});
-    document.dispatchEvent(keyEvent);
+    );
 }
 
 export default SearchBar;
